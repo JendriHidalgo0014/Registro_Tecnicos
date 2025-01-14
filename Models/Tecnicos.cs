dc9ccf6d49746ad445878ec0fce73721d79bd819
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-01-14 14:35:13 -0400

    Creando los modelos.

--- Models/Tecnicos.cs
+++ Models/Tecnicos.cs
@@ -1,6 +1,18 @@
-﻿namespace Registro_Tecnicos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registro_Tecnicos.Models;
+
+
+public class Tecnicos
 {
-	public class Tecnicos
-	{
-	}
+	[Key]
+
+	public int TecnicoId { get; set; }
+
+	[Required(ErrorMessage = "Este campo es requerido")]
+
+	public string Nombres { get; set; } = null!;
+
+	public int SueldoHora { get; set; }
+
 }
