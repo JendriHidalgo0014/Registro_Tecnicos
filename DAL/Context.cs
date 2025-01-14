b52d0a4be1c7063caa41c13a8cc1d88e3924854f
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-01-14 14:41:26 -0400

    Creando el context.

---
+++ DAL/Context.cs
@@ -0,0 +1,12 @@
+﻿using Registro_Tecnicos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Registro_Tecnicos.DAL
+{
+	public class Context : DbContext
+	{
+		public Context(DbContextOptions<Context> options) : base(options) { }
+
+		public DbSet<Tecnicos> Tecnicos { get; set; }
+	}
+}
