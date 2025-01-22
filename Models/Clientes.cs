aac12d1dc74a6d88f69da1e30130e3f50ba5b219
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-01-21 22:02:35 -0400

    Se creó la clase Clientes para agregar sus modelos.

---
+++ Models/Clientes.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Registro_Tecnicos.Models
+{
+	public class Clientes
+	{
+
+		[Key]
+
+		public int ClienteId { get; set; }
+
+		public int TecnicoId { get; set; }
+
+		public DateTime FechaIngreso { get; set; }
+
+		public string Nombres { get; set; }
+
+		public string Direccion {  get; set; }
+
+		public int RNC {  get; set; }
+
+		public decimal LimiteCredito { get; set; }
+
+	}
+}
