e04bb68ffb96f5ce5bf2b65d238bfda59320c401
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-01-28 22:04:23 -0400

    Se inyectó el Service de "Tickets" en el program.

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddBlazorBootstrap();
 //Inyeccion del service.
 builder.Services.AddScoped<TecnicosService>();
 builder.Services.AddScoped<ClientesService>();
+builder.Services.AddScoped<TicketsService>();
 
 var app = builder.Build();
 
