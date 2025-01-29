944161f594ca010ac6ff6bcd3c0241e47d20370e
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-01-28 21:52:21 -0400

    Se crearon los modelos de "Tickets".

---
+++ Models/Tickets.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Registro_Tecnicos.Models
+{
+	public class Tickets
+	{
+		[Key]
+
+		public int TicketId { get; set; }
+
+		public DateTime Fecha {  get; set; }
+
+		public string Prioridad { get; set; }
+
+		[ForeignKey("ClienteId")]
+		public int ClienteId { get; set; }
+
+		public string Asunto {  get; set; }
+
+		public string Descripcion { get; set; }	
+
+		public TimeSpan TiempoInvertido { get; set; }
+
+		[ForeignKey("TecnicoId")]
+
+		public int TecnicoId { get; set; }	
+
+	}
+}
