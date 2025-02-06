4cc72773aab113b1a3a82ed31d646fedeec2036a
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-02-06 16:28:29 -0400

    Se creó la clase Sistemas.

--- Models/Sistemas.cs
+++ Models/Sistemas.cs
@@ -1,6 +1,19 @@
-﻿namespace Registro_Tecnicos.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Registro_Tecnicos.Models
 {
 	public class Sistemas
 	{
+
+		[Key]
+
+		public int SistemaId { get; set; }
+
+		public string Descripcion { get; set; }	
+
+		public string Complejidad { get; set; }
+
 	}
 }
