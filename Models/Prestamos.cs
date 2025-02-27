193f3b51ff7d79411b62a49f08e6173c425fcf0d
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-02-26 22:14:15 -0400

    Se creó Prestamos en el modelo y sus campos.

---
+++ Models/Prestamos.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Registro_Tecnicos.Models
+{
+	public class Prestamos
+	{
+
+		[Key]
+		public int PrestamoId { get; set; }
+
+		public decimal Monto { get; set; }
+
+		public int CantidadCoutas { get; set; }
+
+
+
+	}
+}
