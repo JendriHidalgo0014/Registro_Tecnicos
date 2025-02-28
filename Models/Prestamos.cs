f284ec54e05c0f0db11ddccdcacec7b225fee192
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-02-27 23:36:41 -0400

    Se creó el prestamosdetalle y se agregó tambien en prestamos.

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace Registro_Tecnicos.Models
@@ -18,10 +19,11 @@ namespace Registro_Tecnicos.Models
 		[Required(ErrorMessage = "Este campo es requerido")]
 		[RegularExpression(@"^\d+$", ErrorMessage = "El campo solo puede contener números.")]
 		[Range(1, int.MaxValue, ErrorMessage = "Debe haber al menos 1 cuota")]
-		public int CantidadCuotas { get; set; }	
+		public int CantidadCuotas { get; set; }
 
 
-
+		[ForeignKey("PrestamoId")]
+		public ICollection<PrestamosDetalle> PrestamosDetalle { get; set; } = new List<PrestamosDetalle>();
 
 	}
 }
---
+++ Models/PrestamosDetalle.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Registro_Tecnicos.Models
+{
+	public class PrestamosDetalle
+	{
+
+		[Key]
+
+		public int DetalleId { get; set; }
+
+		[ForeignKey("PrestamoId")]
+		public int PrestamoId { get; set; }
+		public Prestamos Prestamos { get; set; } = null!;
+
+		public int CuotaNo { get; set; }
+
+		public DateTime Fecha { get; set; }
+
+		public decimal Valor { get; set; }
+		
+		public decimal Balance { get; set; }	
+
+
+
+	}
+}
