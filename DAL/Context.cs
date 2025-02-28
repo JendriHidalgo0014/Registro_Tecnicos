0f69f530bc23db5b4eb7585889cf13fdf5259264
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-02-27 23:37:12 -0400

    Se agregó prestamosdetalle en el contexto.

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -18,6 +18,17 @@ namespace Registro_Tecnicos.DAL
 		public DbSet<Prestamos> Prestamos { get; set; }
 
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<PrestamosDetalle>().HasData(new List<PrestamosDetalle>());
+			{
+				new PrestamosDetalle { DetalleId = 1, PrestamoId = 1, CuotaNo = 1, Fecha = DateTime.Now, Valor = 100, Balance = 100 };
+				new PrestamosDetalle { DetalleId = 2, PrestamoId = 1, CuotaNo = 2, Fecha = DateTime.Now, Valor = 100, Balance = 100 };
+				new PrestamosDetalle { DetalleId = 3, PrestamoId = 1, CuotaNo = 3, Fecha = DateTime.Now, Valor = 100, Balance = 100 };
+				new PrestamosDetalle { DetalleId = 4, PrestamoId = 1, CuotaNo = 4, Fecha = DateTime.Now, Valor = 100, Balance = 100 };
+			}
+		}
 
 	}
 }
